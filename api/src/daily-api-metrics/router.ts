3e7cd27ec342650e358e4909d4e8fe5596ce7d45
Alban Mouton <alban.mouton@gmail.com>
2023-02-22 20:57:39 +0100

    feat: use new types builder

--- api/src/daily-api-metrics/router.ts
+++ api/src/daily-api-metrics/router.ts
@@ -1,28 +1,24 @@
 import { Router } from 'express'
 import asyncHandler from 'express-async-handler'
-import { reqBuilder } from '@data-fair/lib/express/req'
-import { type ListResponse, listResponseSchema } from 'types/list-response'
-import { type AggResult, aggResultSchema } from 'types/agg-result'
-import { type AggQuery, aggQuerySchema } from 'types/agg-query'
+import * as listResponseSchema from 'types/list-response'
+import * as aggResultSchema from 'types/agg-result'
+import * as aggQuerySchema from 'types/agg-query'
 
 import { list, agg } from './service'
 
 const router = Router()
 export default router
 
-const listReq = reqBuilder<null, null, ListResponse>(null, null, listResponseSchema)
 router.get('', asyncHandler(async (req, res) => {
   if (!req.session.account) { res.status(401).send(); return }
-  const { send } = listReq(req, res)
   const results = await list(req.session.account)
-  send({ count: results.length, results })
+  res.type('json').send(listResponseSchema.stringify({ count: results.length, results }))
 }))
 
-const aggReq = reqBuilder<AggQuery, null, AggResult>(aggQuerySchema, null, aggResultSchema)
 router.get('/_agg', asyncHandler(async (req, res) => {
   if (!req.session.account) { res.status(401).send(); return }
   if (typeof req.query.split === 'string') req.query.split = req.query.split.split(',')
-  const { query, send } = aggReq(req, res)
+  const query = aggQuerySchema.validate(req.query, req.session.lang, 'query')
   const result = await agg(req.session.account, query)
-  send(result)
+  res.type('json').send(aggResultSchema.stringify(result))
 }))
