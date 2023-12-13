77a0c6708560a8aebec452dfbb1ef5da9da1fac4
Alban Mouton <alban.mouton@gmail.com>
2023-12-13 08:36:41 +0100

    chore: use npm workspaces and latest data-fair lib

--- types/agg-query/index.d.ts
+++
@@ -1,9 +0,0 @@
-export interface AggQuery {
-    start: string;
-    end: string;
-    operationTrack?: "readDataFiles" | "readDataAPI" | "openApplication";
-    statusClass?: "info" | "ok" | "redirect" | "clientError" | "serverError";
-    userClass?: "anonymous" | "owner" | "external" | "ownerAPIKey" | "externalAPIKey" | "ownerProcessing" | "externalProcessing";
-    split?: ("day" | "refererApp" | "processing" | "resource")[];
-}
-export declare const validate: (data: any, lang?: string, name?: string, internal?: boolean) => AggQuery;
