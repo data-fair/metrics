b9938603e3e5ab58ce1ae4bbddb081e96a3aa595
Alban Mouton <alban.mouton@gmail.com>
2024-02-19 12:23:40 +0100

    chore: problems with locale docker network

--- api/config/test.cjs
+++ api/config/test.cjs
@@ -2,5 +2,8 @@ module.exports = {
   port: 6219,
   origin: 'http://localhost:6218',
   directoryUrl: 'http://localhost:6221',
-  mongoUrl: 'mongodb://localhost:27017/metrics-test'
+  mongoUrl: 'mongodb://localhost:27017/metrics-test',
+  prometheus: {
+    active: false
+  }
 }
