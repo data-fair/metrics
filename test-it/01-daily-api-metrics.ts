c7775651ffd977b1902f0729201a454dfddf800d
Alban Mouton <alban.mouton@gmail.com>
2024-10-10 16:15:03 +0200

    fix: waiting for rest dataset finalization after creation is no longer necessary

--- test-it/01-daily-api-metrics.ts
+++ test-it/01-daily-api-metrics.ts
@@ -1,13 +1,11 @@
 import { describe, it, before, after, beforeEach } from 'node:test'
 import { strict as assert } from 'node:assert'
-import { DataFairWsClient } from '@data-fair/lib-node/ws-client.js'
 import * as testSpies from '@data-fair/lib-node/test-spies.js'
 import { axiosAuth, clean, startApiServer, stopApiServer, startDaemonServer, stopDaemonServer } from './utils/index.ts'
 
 testSpies.registerModuleHooks()
 
 const adminAx = await axiosAuth({ email: 'superadmin@test.com', adminMode: true })
-const adminWS = new DataFairWsClient({ url: 'http://127.0.0.1:6218/data-fair/', headers: { Cookie: adminAx.defaults.headers.common.Cookie as string } })
 
 describe('daily api metrics', () => {
   before(startApiServer)
@@ -24,7 +22,6 @@ describe('daily api metrics', () => {
       title: 'd1',
       schema: [{ key: 'prop1', type: 'string' }]
     })).data
-    await adminWS.waitForJournal(dataset.id, 'finalize-end')
     const [rawLine, [day, parsedLine]] = await Promise.all([
       testSpies.waitFor('rawLine'),
       testSpies.waitFor<[string, string]>('parsedLine'),
